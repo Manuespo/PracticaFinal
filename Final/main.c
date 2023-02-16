fa9f19e5c7ba292a95985b688bd80b2f458a50ae
Manuespo <manuelesposito@outlook.com>
2023-02-16 11:18:38 -0300

    creo estructuras de datos

--- Final/main.c
+++ Final/main.c
@@ -2,9 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct
+{
+    char nombre[20];
+    char apellido[20];
+    int edad;
+    char deporte[20];
+    int idDeporte;
+}stRegistro;
+
+typedef struct
+{
+    char nombre[20];
+    char apellido[20];
+    int edad;
+}deportista;
+typedef struct
+{
+    deportista dato;
+    struct nodoDeportista* siguiente;
+}nodoDeportista;
+
+typedef struct
+{
+    char deporte[20];
+    int idDeporte;
+    nodoDeportista * listaDeportistas;
+    struct nodoArbol* izq;
+    struct nodoArbol* der;
+}nodoArbol;
 
 int main()
 {
-    printf("Hello world!\n");
+
     return 0;
 }
